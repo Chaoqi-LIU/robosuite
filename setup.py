7994219925b7c41b22f749c2c1e7fd66ab6a4fe2
Chaoqi Liu <60335143+Chaoqi-LIU@users.noreply.github.com>
2025-09-18 16:36:27 -0400

    Change numpy version requirement to 1.23.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name="robosuite",
     packages=[package for package in find_packages() if package.startswith("robosuite")],
     install_requires=[
-        "numpy>=1.13.3",
+        "numpy==1.23.3",
         "numba>=0.49.1",
         "scipy>=1.2.3",
         "mujoco==3.2.6",
